263db58b67562da12b5057e518f21ee7017749c8
Weiping HUANG <Nightonke@outlook.com>
2016-04-01 17:47:37 +0800

    Icons

---
+++ app/src/main/java/com/nightonke/saver/model/CoCoin.java
@@ -0,0 +1,11 @@
+package com.nightonke.saver.model;
+
+/**
+ * Created by Weiping on 2016/1/21.
+ */
+
+public class CoCoin {
+
+    public static String APPLICATION_ID = "9d30e7692a56a79d7600b95cbff43283";
+
+}
